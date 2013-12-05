ff9f3ad790acbb9ab0572379d6fc3f66e0dfd012
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 11:34:58 -0500

    Added the problem as a comment.

--- Average shopping bill/js/average_shopping_bill.js
+++ Average shopping bill/js/average_shopping_bill.js
@@ -1,3 +1,9 @@
 //// Marlon Torres - December 5, 2013 - Web Programming Fundamentals - Average shopping bill
 
 
+//You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the
+//past five weeks. Store the past five grocery totals as a list in an array. Create an expression that will use the
+// items in the array to calculate the average amount spent on groceries. Please note: there should only be one given
+//for this problem set, the array holding the five weekly totals.  You should be able to access the array using the
+//array access notation discussed earlier in the course.
+
