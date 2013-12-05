13be77c54352ff9fd224f2917162ee63d76ad986
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 12:53:08 -0500

    Changed order of operation in expression by adding the () and the then dividing by 5. Added comments as well.

--- Expressions_worksheet/Average shopping bill/js/average_shopping_bill.js
+++ Expressions_worksheet/Average shopping bill/js/average_shopping_bill.js
@@ -7,9 +7,11 @@
 //for this problem set, the array holding the five weekly totals.  You should be able to access the array using the
 //array access notation discussed earlier in the course.
 
-var wkGrocerytotals = [40, 60, 35, 80, 100];
+var wkGrocerytotals = [40, 60, 35, 80, 100]; //declaring and defining variable with array.
 
-var weeklyAVG = wkGrocerytotals [0] + wkGrocerytotals [1] + wkGrocerytotals [2] + wkGrocerytotals [3] + wkGrocerytotals[4];
-console.log(weeklyAVG);
+var weeklyAVG = (wkGrocerytotals [0] + wkGrocerytotals [1] + wkGrocerytotals [2] + wkGrocerytotals [3] + wkGrocerytotals[4]) / 5;
+//This variable holds the total of the weekly grocery shopping, adds them up and then divides them by 5 (last five weeks). The numbers
+//in the brackets are the index numbers assigned to the item we want to access.
+console.log(weeklyAVG); //prints out the anwser in the console.
 
 
