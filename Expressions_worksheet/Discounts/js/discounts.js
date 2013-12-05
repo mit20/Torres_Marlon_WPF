28455c19fd4294b698294d6040ccb1f05bb4cdd9
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 13:14:12 -0500

    Initial setup of discounts’ problem js file.

---
+++ Expressions_worksheet/Discounts/js/discounts.js
@@ -0,0 +1,6 @@
+// Marlon Torres - December 5, 2013 - Web Programming Fundamentals - Discounts Problem
+
+// Calculate the discounted price for an item. Create an expression that will calculate the discounted price with
+// and without sales tax. (It is acceptable for the result to have more than two digits after the decimal. $345.896 for example.)
+
+
