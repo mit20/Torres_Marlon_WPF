0ebfc6dc28873ff917fc93b1ec10f5bfae288150
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 00:53:00 -0500

    Added comments for every line of code

--- Expressions_worksheet/Slice Pizza Part 1/js/slicepizza1.js
+++ Expressions_worksheet/Slice Pizza Part 1/js/slicepizza1.js
@@ -5,8 +5,8 @@
 //(Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise,
 //so this can be a decimal, like 3.52 slices, etc.)
 
-var slicesPerpizza = 10;
-var guests = 23;
-var pizzasOrdered = 10;
-var slicesPerguest = slicesPerpizza * pizzasOrdered/guests;
-console.log("Each person ate" + " "+ slicesPerguest + " " + "slices of pizza at the party.");
+var slicesPerpizza = 10; //Defines and declares slices per pizza.
+var guests = 23; //Defines and declares total amount of students at party.
+var pizzasOrdered = 10; //Defines and declares total amount of pizzas ordered.
+var slicesPerguest = slicesPerpizza * pizzasOrdered/guests; //Defines and declares how many slices each guest will have.
+console.log("Each person ate" + " "+ slicesPerguest + " " + "slices of pizza at the party."); //Prints to console
