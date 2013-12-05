e6c1f7960dc49567bf95c66bf1f87cf0f127e298
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 12:57:12 -0500

    Not sure where my files went on GitHub, so I am uploading again. (js file)

---
+++ Expressions_worksheet/Dog Years Problem/js/dogyears_scipt.js
@@ -0,0 +1,15 @@
+// Marlon Torres - December 4, 2013 - Web Programming Fundamentals - Dog Years Problem
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age. Dogs age 7 times faster than humans
+// so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull
+// is in dog years based on his actual age.
+
+var humanYear = 1; //Given 1 equals a human year.
+var dogYears = 7;  //Given that 7 dog years are equal to 1 human year.
+var sparkyAge = humanYear * dogYears; //Expression that calculates Sparky's Age.
+console.log("Sparky is" + " " + humanYear + " " +  "human years old which is" + " " + sparkyAge +" " + "in dog years.");
+//Line 10 prints out the result in the console.
+
+
+
+
