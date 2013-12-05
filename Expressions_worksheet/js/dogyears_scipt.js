2e16d04fc4d1b49e4572b811511e6ba9d58a44ff
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-04 22:05:30 -0500

    Defined and declared my first variable.

--- Expressions_worksheet/js/dogyears_scipt.js
+++ Expressions_worksheet/js/dogyears_scipt.js
@@ -1,1 +1,11 @@
 // Marlon Torres - December 4, 2013 - Web Programming Fundamentals - Dog Years Problem
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age. Dogs age 7 times faster than humans
+// so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull
+// is in dog years based on his actual age.
+
+var humanYear = 1; //Given 1 equals a human year.
+
+
+
+
