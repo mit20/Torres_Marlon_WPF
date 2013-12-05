a6fbf871d26a9351027374de3146c39b387287e8
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-05 01:25:15 -0500

    Initial setup of Slice Pizza Part 2 problem.

---
+++ Expressions_worksheet/Slice Pizza Part 2/js/slicepizza2.js
@@ -0,0 +1,8 @@
+// Marlon Torres - December 4, 2013 - Web Programming Fundamentals - Slice Pizza Part 2 problem
+
+//At the pizza party Sparky, the host’s dog is excited, because he gets the leftover pizza after the slices have been
+//divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky feast on?
+//Example data set: 10 people, 4 pizzas and 8 slices per pizza will mean each person eats 3 slices and Sparky gets 2
+//slices. (Note that this is an example, your code should work and give me the accurate results no matter what numbers
+//I put in for those given variables.)
+
