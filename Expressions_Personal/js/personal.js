07eae2a8b9818a6564b155762788723b68290bd8
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 15:35:19 -0500

    Added name, date and assigment. I also added and alert to let the user know what the problem is and added length, width and depth variables.

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -1,3 +1,7 @@
-/**
- * Created by Marlon on 12/6/13.
- */
+// Marlon Torres - December 6, 2013 - Web Programming Fundamentals - Expressions Personal Problem
+
+alert("We are calculating how many gallons of water your pool uses.");
+
+var width = prompt("Enter the width of your pool:");
+var length = prompt("Enter the length of your pool:");
+var depth = prompt("Enter the depth of your pool:");
