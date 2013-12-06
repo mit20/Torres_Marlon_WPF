6d239b2856be9455da05be055b38b139a7f1aada
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 16:44:23 -0500

    Deleted some unnecessary console lines and added comments to the last line.  Also correct spelling of the word formula in line 9.

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -4,19 +4,16 @@
 alert("We are calculating how many gallons of water your pool uses.");//This will let the user know what we are calculating in the browser.
 
 var width = prompt("Enter the width of your pool:");//This is the width variable that will prompt the user to enter it.
-console.log("Width entered")//Prints to console and shows "Width entered"
-console.log(width);//Prints to console the actual width that was entered.
 var length = prompt("Enter the length of your pool:");//This is the length variable that will prompt the user to enter it.
 var depth = prompt("Enter the depth of your pool:");//This is the depth variable that will prompt the user to enter it.
-var cubicFeet = width * length * depth;//This expression declares and defines the variable for the forumula that calculates the cubic feet.
+var cubicFeet = width * length * depth;//This expression declares and defines the variable for the formula that calculates the cubic feet.
 var gallons = cubicFeet * 7.47;//This expression declares and defines the variable for the formula that calculates how many gallons of water is need for X amount of cubic feet.
 alert("Based on the information you have entered, \nyour pool is" + " " + cubicFeet + " " + "cubic feet and uses" + " " + gallons + " " +"gallons of water.");//Alert that shows the results and my string is concatenated (concatenated expression).
 
-console.log("Width entered")//Prints to console and shows "Width entered"
-console.log(width);//Prints to console the actual width that was entered.
-console.log("Length entered")//Prints to console and shows "Length entered"
-console.log(length);//Prints to console the actual length that was entered.
-console.log("Depth entered")//Prints to console and shows "Depth entered"
-console.log(depth);//Prints to console the actual depth that was entered.
-console.log("Cubic feet of pool")//Prints to console and shows "Cubic feet of pool"
-console.log(cubicFeet);//Prints to console the actual cubic feet that was calculated.
+console.log("Width entered" + " " + width + " " + "feet");//Prints to console and shows "Width entered"
+
+console.log("Length entered" + " " + length + " " + "feet")//Prints to console and shows "Length entered"
+
+console.log("Depth entered" + " " + depth + " " + "feet")//Prints to console and shows "Depth entered"
+
+console.log("Based on the information you have entered, \nyour pool is" + " " + cubicFeet + " " + "cubic feet and uses" + " " + gallons + " " +"gallons of water.");//Prints result with explanation to console.
