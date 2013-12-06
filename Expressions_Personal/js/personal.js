3a1860dd2b8855177edeb9c50f7f8d712c23e429
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 15:38:55 -0500

    Added comments to the variable lines and explained what they and the prompts do.

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -1,7 +1,8 @@
 // Marlon Torres - December 6, 2013 - Web Programming Fundamentals - Expressions Personal Problem
+//
 
-alert("We are calculating how many gallons of water your pool uses.");
+alert("We are calculating how many gallons of water your pool uses.");//This will let the user know what we are calculating in the browser.
 
-var width = prompt("Enter the width of your pool:");
-var length = prompt("Enter the length of your pool:");
-var depth = prompt("Enter the depth of your pool:");
+var width = prompt("Enter the width of your pool:");//This is the width variable that will prompt the user to enter it.
+var length = prompt("Enter the length of your pool:");//This is the length variable that will prompt the user to enter it.
+var depth = prompt("Enter the depth of your pool:");//This is the depth variable that will prompt the user to enter it.
