0e4f40717c0a33ba1dba7d22b807efaf7e09a6cf
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 18:12:42 -0500

    Initial setup of the Javascript file for the Expressions Industry problem.  Added name, date and class up top and created an Alert that will advise the user how this calculator works.

---
+++ Expressions_Industry/js/industry.js
@@ -0,0 +1,5 @@
+//// Marlon Torres - December 6, 2013 - Web Programming Fundamentals - Expressions Industry Problem
+// I created this problem to convert Kilobytes to Megabytes, and Megabytes to Gigabytes.
+
+alert("This calculator converts Kilobytes to Megabytes and Megabytes to Gigabytes")
+
