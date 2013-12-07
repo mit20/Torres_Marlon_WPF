58e66d6f6c8d142547a12808d837f77410f6d526
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 21:59:17 -0500

    Added consoles so that the variables would print to the console and added comments to those lines.  Changed newheight variable to newHeight.

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -5,9 +5,15 @@ alert("Resize your image without it affecting its aspect ratio.");//Alerts user 
 var originalWidth = prompt("Enter the original width of your image in pixels:"); //Declares and defines originalWidth variable and prompts the user to enter the original width of the image with message in the string.
 var originalHeight = prompt("Enter the original height of your image in pixels:"); //Declares and defines originalHeight variable and prompts the user to enter the original height of image with message in the string.
 var newWidth = prompt("Enter the new width you desire in pixels:");  //Declares and defines newWidth variable and prompts the user to enter new width with message in the string.
-var newheight = (originalWidth / originalWidth) * newWidth; //Declared and defined the newHeight variable and created formula to find new height with arithmetic operator expression.
-var result = ("Therefore, your resized image is" + " " + newheight  + " " + "pixels high" + " "+ "by" + " " + newWidth + " " + "wide." );//Declared and defined result variable and added concatenating string message.
+var newHeight = (originalHeight / originalWidth) * newWidth; //Declared and defined the newHeight variable and created formula to find new height with arithmetic operator expression.
+var result = ("Therefore, your resized image is" + " " + newHeight  + " " + "pixels high" + " "+ "by" + " " + newWidth + " " + "wide." );//Declared and defined result variable and added concatenating string message.
 alert(result);//Alerts user of the result in browser
+
+console.log(originalWidth);//prints to console
+console.log(originalHeight);//prints to console
+console.log(newWidth);//prints to console
+console.log(newHeight);//prints to console
+console.log(result);//prints to console
 
 
 
