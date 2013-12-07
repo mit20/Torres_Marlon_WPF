c572ac0fd72f6cb30d47c8e65390de9d0033147e
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-06 21:07:27 -0500

    Declared and defined the variables and added comments to them.

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -1,6 +1,10 @@
 //// Marlon Torres - December 6, 2013 - Web Programming Fundamentals - Expressions Industry Problem
 // This calculator will resize an image and maintain its aspect ratio.
 
+var originalWidth = prompt("Enter the original width of your image in pixels:"); //Declares and defines originalWidth variable and prompts the user to enter the original width of the image.
+var originalHeight = prompt("Enter the original height of your image in pixels:"); //Declares and defines originalHeight variable and prompts the user to enter the original height of image.
+var newWidth = prompt("Enter the new width you desire in pixels:");  //Declares and defines newWidth variable and prompts the user to enter new width.
+
 
 
 
