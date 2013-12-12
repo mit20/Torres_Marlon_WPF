dfab7d4ab933e610b5b253ab1079b348139d4490
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 10:39:40 -0500

    Added code and comments.

--- Conditionals_worksheet/celsiustofahrenheit/js/celsiustofahrenheit.js
+++ Conditionals_worksheet/celsiustofahrenheit/js/celsiustofahrenheit.js
@@ -5,4 +5,16 @@
 //calculator should convert to Fahrenheit.
 
 
+var temp = prompt("Enter temperature:");//Requests actual temperature that user wants converted.
+var celOrFahren = prompt("Enter C or F.");//Requests the letter C for Celsius or F for Fahrenheit.
 
+//If the temperature is in celsius(c) then it converts to fahrenheit, otherwise it will convert to celsius.
+if(celOrFahren == "c"){
+    //Converts celsius to fahrenheit.
+    var cel=parseFloat(temp)*(9/5)+32;
+    console.log("The temperature is " +cel+ " degrees Fahrenheit.");//prints to console
+}else {
+    //Converts from fahrenheit to celsius if f was entered.
+    var cel=(parseFloat(temp)-32)*(5/9);
+    console.log("The temperature is " + cel + " degrees Celsius.")//prints to console
+}
