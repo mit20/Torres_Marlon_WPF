b367291f5a1dd733e6b53a893a6a915d69eed9d2
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 10:46:47 -0500

    Initial setup of JavaScript file for Grade Calculator problem.

---
+++ Conditionals_worksheet/grades/js/grades.js
@@ -0,0 +1,4 @@
+//Marlon Torres - December 11, 2013 - Web Programming Fundamentals - Grade Letter Calculator
+
+//A student earns a number grade at the conclusion of a course at Full Sail.  Determine the appropriate letter
+//grade for that number using conditional statements.
