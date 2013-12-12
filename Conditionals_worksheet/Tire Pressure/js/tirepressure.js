10d9c8a5253f90c01774e1610b4de8f337434289
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 17:35:08 -0500

    Initial setup of JavaScipt file fork Tire Pressure I problem.

---
+++ Conditionals_worksheet/Tire Pressure/js/tirepressure.js
@@ -0,0 +1,8 @@
+//Marlon Torres - December 11, 2013 - Web Programming Fundamentals - Tire Pressure I
+
+//To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires
+//should have the same pressure.  But the front tires and the rear tires can have different pressure than each other,
+//so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine
+//if the tires of a given car are up to spec.
+
+
