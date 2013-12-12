7cf03576d9384f844a8039e024af0292443a9bda
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 10:24:41 -0500

    Initial setup for Javascript file for the Celsius to Fahrenheit converter problem.

---
+++ Conditionals_worksheet/celsiustofahrenheit/js/celsiustofahrenheit.js
@@ -0,0 +1,8 @@
+//Marlon Torres - December 11, 2013 - Web Programming Fundamentals - Celsius to Fahrenheit converter.
+
+//Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the
+//user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the
+//calculator should convert to Fahrenheit.
+
+
+
