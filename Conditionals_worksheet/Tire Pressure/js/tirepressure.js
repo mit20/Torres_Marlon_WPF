8017123adec68d1deb514fcc9fe1e9fc4b76a833
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 21:25:32 -0500

    Added an array for the tires and also added the If statements to show multiple conditions along with the comments.

--- Conditionals_worksheet/Tire Pressure/js/tirepressure.js
+++ Conditionals_worksheet/Tire Pressure/js/tirepressure.js
@@ -1,8 +1,28 @@
 //Marlon Torres - December 11, 2013 - Web Programming Fundamentals - Tire Pressure I
 
-//To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires
+//To meet maintenance standards a car’s front two tires should have the same pressure and the back two tires
 //should have the same pressure.  But the front tires and the rear tires can have different pressure than each other,
 //so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine
 //if the tires of a given car are up to spec.
+
+
+var tireArray =["37","37", "38", "38"];//Array for tires
+
+//If statements that demonstrate multiple conditions using the Logical Operator &&.
+
+if(tireArray[0] == tireArray[1] && tireArray[2] == tireArray[3]){
+    console.log("The tires pass spec")//print to console
+}if(tireArray[0] != tireArray[1] && tireArray[2] == tireArray[3] ){
+    console.log("Get your tires checked out!");//print to console
+}if (tireArray[0] == tireArray[1] && tireArray[2] != tireArray[3] ){
+    console.log("Get your tires checked out!");//print to console
+}
+
+
+
+
+
+
+
 
 
