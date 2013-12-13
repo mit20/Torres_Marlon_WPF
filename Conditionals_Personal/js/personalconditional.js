957890788e14896121c63bfcc0958af68d282659
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-12 22:40:30 -0500

    Initial setup of JavaScript file for the Conditionals Personal problem.

---
+++ Conditionals_Personal/js/personalconditional.js
@@ -0,0 +1,10 @@
+//Marlon Torres - December 12, 2013 - Web Programming Fundamentals - Conditional - Personal
+//You will create three separate projects, each a set of files and folders. Each set will be a calculator that collects
+//information from the user with prompts, calculates some sort of useful result with that information and print a result
+//to the console.
+//Personal: The first set should be something personal to you. Some examples
+
+//This problem will help you decide if it is better for you to move to Atlanta or Miami based on employment opportunity.
+
+
+
