a58600d38d84125da21d75bbcd4abdc4e42c2053
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-14 22:18:18 -0500

    Added console.log line with print comment.

--- Functions_worksheet/js/functions_script.js
+++ Functions_worksheet/js/functions_script.js
@@ -1,2 +1,7 @@
 //Marlon Torres - December 14, 2013 - Web Programming Fundamentals - Functions Worksheet
 //Calculate the circumference of a circle.
+
+
+
+
+console.log("The circumference of the circle is X"); //prints to console.
