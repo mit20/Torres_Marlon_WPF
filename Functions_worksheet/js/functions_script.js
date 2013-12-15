e0bcc44d1f8612c6417340255874988e018cfa5e
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-15 10:35:32 -0500

    Add the arguments and parameters of the function.  Defined the function.

--- Functions_worksheet/js/functions_script.js
+++ Functions_worksheet/js/functions_script.js
@@ -1,7 +1,12 @@
 //Marlon Torres - December 14, 2013 - Web Programming Fundamentals - Functions Worksheet
 //Calculate the circumference of a circle.
 
+circumference(10, 3.14); //These are the arguments. Invokes function aka function call.
+
+function circumference(diameter, pi){ //Parameters - storage
+    var calcCircum = diameter * pi;
+    console.log("The circumference of the circle is " + calcCircum); //prints to console.
+}
 
 
 
-console.log("The circumference of the circle is X"); //prints to console.
