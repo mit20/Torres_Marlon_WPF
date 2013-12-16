1af3b9511b97e7039945473646fc7d5bae3ac3e6
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-16 16:28:16 -0500

    Added function definition.  Includes function name, parameters  and code that executes when the function is called.

--- Functions_worksheet/Stung/js/stung_script.js
+++ Functions_worksheet/Stung/js/stung_script.js
@@ -2,5 +2,9 @@
 //It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an
 //animal in a function.
 
+function beeStings(animalWeight){
+    var beeStings = animalWeight * 8.666666667;
+    console.log("It takes " + beeStings + " bee stings to kill this animal.");
+}
 
 
