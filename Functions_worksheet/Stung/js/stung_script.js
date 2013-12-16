395892074e1198bd860624570aa89afcf11dbf52
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-16 17:16:55 -0500

    Added comments, changed variable name from beeStings to calcBeeStings and added return line.

--- Functions_worksheet/Stung/js/stung_script.js
+++ Functions_worksheet/Stung/js/stung_script.js
@@ -2,12 +2,14 @@
 //It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an
 //animal in a function.
 
+var animalWeight = 200;//Defines and declares the value of the variable (given).
+var totalStings = beeStings(animalWeight);//Invokes function (calls it). animalWeight is an argument with the value of 200 and is passed to the function.
+
 function beeStings(animalWeight){//function name "beeStings". Variable animalWeight is setup inside the parameter section.
-    var beeStings = animalWeight * 8.666666667; //calculates the animal weight by the bee stings per pound.
-    console.log("It takes " + beeStings + " bee stings to kill this animal.");//prints out to console.
+    var calcBeeStings = animalWeight * 8.666666667; //calculates the animal weight by the bee stings per pound.
+    return calcBeeStings;
 }
 
-var animalWeight = 200;//Defines and declares the value of the variable.
-beeStings(animalWeight);//Invokes function (calls it).
+console.log("It takes " + totalStings + " bee stings to kill this animal.");//prints out to console.
 
 
