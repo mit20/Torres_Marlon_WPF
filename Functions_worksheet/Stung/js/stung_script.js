8f5303d9b05a64e9aa78cf1bea2c49bedabe5008
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-16 16:25:45 -0500

    Added problem to the comments at the top of the file.

--- Functions_worksheet/Stung/js/stung_script.js
+++ Functions_worksheet/Stung/js/stung_script.js
@@ -1,3 +1,6 @@
 //Marlon Torres - December 16, 2013 - Web Programming Fundamentals - Functions Worksheet - Stung Problem
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an
+//animal in a function.
+
 
 
