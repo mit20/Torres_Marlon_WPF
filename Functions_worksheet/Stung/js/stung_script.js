752356a5d522dd9efdb7cb4ebf0bd6986bbf2846
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-16 16:32:57 -0500

    Added comments to lines 5, 6 and 7 respectively.

--- Functions_worksheet/Stung/js/stung_script.js
+++ Functions_worksheet/Stung/js/stung_script.js
@@ -2,9 +2,9 @@
 //It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an
 //animal in a function.
 
-function beeStings(animalWeight){
-    var beeStings = animalWeight * 8.666666667;
-    console.log("It takes " + beeStings + " bee stings to kill this animal.");
+function beeStings(animalWeight){//function name "beeStings". Variable animalWeight is setup inside the parameter section.
+    var beeStings = animalWeight * 8.666666667; //calculates the animal weight by the bee stings per pound.
+    console.log("It takes " + beeStings + " bee stings to kill this animal.");//prints out to console.
 }
 
 
