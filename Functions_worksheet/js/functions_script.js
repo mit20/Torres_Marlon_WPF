012f06d28353239aa0105430faf29124579eb875
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-16 12:54:54 -0500

    Made changes to have a value passed on and also changed some of the comments.

--- Functions_worksheet/js/functions_script.js
+++ Functions_worksheet/js/functions_script.js
@@ -1,15 +1,10 @@
 //Marlon Torres - December 14, 2013 - Web Programming Fundamentals - Functions Worksheet
 //Calculate the circumference of a circle.
 
-var total = circumference(10, 3.14); //These are the arguments. Invokes function aka function call. Where the returned
-//information is stored is established here. In this case it will be stored in the variable total.
-
-function circumference(diameter, pi){ //Parameters - storage
-    var calcCircum = diameter * pi;
-    return calcCircum;//return keyword will get the information out of the function. This will return the calcCircum
-
+var total = circumference(30);//function is invoked here. This is where the parameters are held. 30 is passed into
+//function (diameter). Variable total stores what is sent from calcCircum.
+function circumference(diameter){ //diameter variable stores the value that is passed in.
+    var calcCircum = diameter * 3.14;//formula for circumference
+    return calcCircum;//Used keyword return for it to Spit out this information.
 }
-
 console.log("The circumference of the circle is " + total); //prints to console.
-
-
