fc511a40bbd38ecce1b26f7bd7e59aae326c479e
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-18 16:12:15 -0500

    Changed comments in lines 8, 9 and 10. Changed the word calculated to divided.

--- Functions_Personal/js/function_personal.js
+++ Functions_Personal/js/function_personal.js
@@ -5,17 +5,17 @@ var salaryGlobal = prompt("What is your annual net salary?", " ");//Declared var
 var payFrequencyGlobal = prompt("How often are you paid? Enter W for weekly, B for biweekly or M for monthly.");//Declared variable and prompt asks user to enter how often they are paid.
 var percentToSaveGlobal = 0.2;
 
-function howMuchToSave(salary, payFrequency, percentToSave){//function named howMuchToSave with parameters inside the parenthesis.
-    var W = 52;//variable W is for weeks, so it will calculate by 52 if W is inputted.
-    var B = 26;//variable B is for weeks, so it will calculate by 26 if W is inputted.
-    var M = 12;//variable M is for weeks, so it will calculate by 12 if W is inputted.
+function howMuchToSave(salaryGlobal, payFrequencyGlobal, percentToSaveGlobal){//function named howMuchToSave with parameters inside the parenthesis.
+    var W = 52;//variable W is for weeks, so it will divided by 52 if W is inputted.
+    var B = 26;//variable B is for weeks, so it will divided by 26 if W is inputted.
+    var M = 12;//variable M is for weeks, so it will divided by 12 if W is inputted.
     if (payFrequencyGlobal == W){//If statement with condition and equality operator ==.  If Variable payFrequencyGlobal is equal to W then it will continue to perform action.
-        return (salary * percentToSave) / W+ " add text here";//returns or spits out this information (calculation) out of the function.
-    }else if(payFrequency == B){//Else if statment with condition and equality operator ==.  If Variable payFrequencyGlobal is equal to B then it will continue to perform action. otherwise it will move on to next process.
-        return salary * percentToSave / B+ " add text here";
+        return "You will need to save" + salaryGlobal * percentToSaveGlobal / W+ " dollars a week.";//returns or spits out this information (calculation) out of the function.
+    }else if(payFrequencyGlobal == B){//Else if statment with condition and equality operator ==.  If Variable payFrequencyGlobal is equal to B then it will continue to perform action. otherwise it will move on to next process.
+        return (salaryGlobal * percentToSaveGlobal) / B+ " add text here";
     }else{//Else if statment with condition and equality operator ==.  If Variable payFrequencyGlobal is equal to M then it will continue to perform action. otherwise it will move on to next process.
-        return salary * percentToSave / M+ " add text here";
-    }
-}
+    return (salaryGlobal * percentToSaveGlobal) / M+ " add text here";
 
-console.log(howMuchToSave(salaryGlobal,payFrequencyGlobal, percentToSaveGlobal));//Prints to console.
+}
+}
+console.log(howMuchToSave(salaryGlobal, payFrequencyGlobal, percentToSaveGlobal));//Prints to console.
