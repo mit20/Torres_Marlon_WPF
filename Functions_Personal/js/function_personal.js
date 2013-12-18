407697596cdc45f6b93180eb9400e9c3389e723a
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-18 00:33:10 -0500

    Changed explanation of problem in the comments section up top.

--- Functions_Personal/js/function_personal.js
+++ Functions_Personal/js/function_personal.js
@@ -1,2 +1,4 @@
 //Marlon Torres - December 17, 2013 - Web Programming Fundamentals - Functions Personal
-//This problem allows the user to calculate what they need to save per pay period in order to save 20% annual net salary.
+//This problem allows the user to calculate how much they need to save per pay period in order to save 20% of their annual net salary.
+
+
