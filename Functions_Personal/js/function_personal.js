96465dac33c63b9ff2660f54dfd999cfa6f5cebb
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-18 11:30:45 -0500

    Added comments to line 4, 5 and 12.  Added condition to if statement with equality operator.

--- Functions_Personal/js/function_personal.js
+++ Functions_Personal/js/function_personal.js
@@ -1,15 +1,15 @@
 //Marlon Torres - December 17, 2013 - Web Programming Fundamentals - Functions Personal
 //This problem allows the user to calculate how much they need to save per pay period in order to save 20% of their annual net salary.
 
-var salaryGlobal = prompt("What is your annual net salary?", " ");//Ask user to enter their annual net salary.
-var payFrequencyGlobal = prompt("How often are you paid? Enter W for weekly, B for biweekly or M for monthly.");
+var salaryGlobal = prompt("What is your annual net salary?", " ");//Declared variable and prompt asks user to enter their annual net salary.
+var payFrequencyGlobal = prompt("How often are you paid? Enter W for weekly, B for biweekly or M for monthly.");//Declared variable and prompt asks user to enter how often they are paid.
 var percentToSaveGlobal = 0.2;
 
 function howMuchToSave(){
     var W = 52;
     var B = 26;
     var M = 12;
-    if (payFrequencyGlobal == W){
+    if (payFrequencyGlobal == W){//If statement with condition and equality operator ==.  If Variable payFrequencyGlobal is equal to W then it will continue to perform action.
 
     }
 }
