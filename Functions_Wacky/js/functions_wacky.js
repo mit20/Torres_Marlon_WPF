95ad7c14e4c11d902ce6d2436eaaa3af99b9761a
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:10:16 -0500

    Added first variable with comments.

--- Functions_Wacky/js/functions_wacky.js
+++ Functions_Wacky/js/functions_wacky.js
@@ -1,3 +1,4 @@
 //Marlon Torres - December 19, 2013 - Web Programming Fundamentals - Functions Wacky
 //Problem: You're a waiter/waitress at a restaurant. Calculate what your average tip and if its a good or bad shift in terms of tips.
 
+var tipsGlobal = prompt("What is the number of tips you received today?", " ");//declares variable and asks user to enter amount of tips
