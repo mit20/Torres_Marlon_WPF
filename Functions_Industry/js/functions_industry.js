56f837b498d4f4aea23a86f9a50fdf1080501e8d
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 23:13:16 -0500

    Added comment to line 9.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -6,5 +6,4 @@ alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyt
 var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? Please enter a number only.", " ");//declared and defined variable. Prompt asks user question for he/she to input.
 var bitrates = prompt("What bit rates are your songs?");//declared and defined variable bitrates. Prompt asks user question.
 var averageSongLength =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
-var songsPerGigabyte = 256;
-
+var songsPerGigabyte = 256;//variable is for songs per Gigabyte.  1024kb = 1 mb Avg. song is 4 min. so 1024/4 = 256.
