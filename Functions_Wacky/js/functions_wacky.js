653f53d4e8f01e12039437f3264b2c02b98abc17
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:21:52 -0500

    Added function

--- Functions_Wacky/js/functions_wacky.js
+++ Functions_Wacky/js/functions_wacky.js
@@ -5,5 +5,6 @@ var tipsGlobal = prompt("What is the number of tips you received today?", " ");/
 (tipsGlobal <= 5)? alert("Wow that's pretty bad!") : alert("That's not too bad!");//conditional statement - ternary.
 
 var moneyGlobal = prompt("What is the dollar amount you made in tips today?", " ");//declared and defined variable.  prompt asks user to enter the dollar amount they earned.
-
+var calcTips = function (tips,money){ //function with its two parameters.
+}
 
