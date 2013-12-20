1e768e44099ba65179035210d1a1929e472b0dcc
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-20 00:33:41 -0500

    Edited comment on line 2 and edited alerts on line 4 & 5.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -1,8 +1,8 @@
 //Marlon Torres - December 18, 2013 - Web Programming Fundamentals - Functions Industry
-//Problem: Calculate how many songs will fit on a 16 gigabyte iPod.
+//Problem: Calculate how many songs your iPod will hold based on the capacity of your iPod and bit rate of your songs.
 
-alert("This calculator will calculate how many songs your iPod can hold based the capacity of your iPod and bit rate of your songs.");//Shows this message to user.
-alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute.");//Alerts user of bit rate information.
+alert("This calculator will calculate how many songs your iPod can hold based on the capacity of your iPod and bit rate of your songs.");//Shows this message to user.
+alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute).");//Alerts user of bit rate information.
 var songs = prompt("How many songs would you like to load on to your iPod?", " ");
 var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? Please enter a number only.", " ");//declared and defined variable. Prompt asks user question for he/she to input.
 var bitrate = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bitrate. Prompt asks user question.
