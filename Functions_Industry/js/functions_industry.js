bcee8ccda4c5543164a187281da33fd83259ed83
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-20 01:20:44 -0500

    Added text to the returns on lines 19 & 21.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -5,7 +5,7 @@ alert("This calculator will calculate how many songs your iPod can hold based on
 alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute).");//Alerts user of bit rate information.
 var songsGlobal = prompt("How many songs would you like to load on to your iPod?", " ");
 var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? Please enter a number only.", " ");//declared and defined variable. Prompt asks user question for he/she to input.
-var bitrateGlobal = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bitrate. Prompt asks user question.
+var bitrateGlobal = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bit rate. Prompt asks user question.
 var averageSongLengthGlobal =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
 var songsPerGigabyte = 256;//variable is for songs per Gigabyte.  1024kb = 1 mb Avg. song is 4 min. so 1024/4 = 256.
 
@@ -16,12 +16,14 @@ function calcSongs(songs, averageSongLength, bitrate){
     var mbTwo = 2;
 
     if(bitrate == 128){
-        return "If you upload " + songs + " it will take up " + (songs * averageSongLength) * mbOne + " megabytes.";
+        return "If you upload " + songs + " songs, it will take up " + (songs * averageSongLength) * mbOne + " megabytes.";
     }else if(bitrate == 256){
-        return "If you upload " + songs + " it will take up " + (songs * averageSongLength) * mbTwo + " megabytes.";
+        return "If you upload " + songs + " songs, it will take up " + (songs * averageSongLength) * mbTwo + " megabytes.";
     }else{
-
+        return "You must enter 128 or 256.";
     }
 }
 
 console.log(calcSongs(songsGlobal, averageSongLengthGlobal, bitrateGlobal));//Prints to console.
+
+
