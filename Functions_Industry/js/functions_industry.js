53e0bd49f62cf2619375efd31bdd74ed80f7a492
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 23:43:54 -0500

    Added ternary.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -8,6 +8,6 @@ var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? P
 var bitrate = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bitrate. Prompt asks user question.
 var averageSongLength =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
 var songsPerGigabyte = 256;//variable is for songs per Gigabyte.  1024kb = 1 mb Avg. song is 4 min. so 1024/4 = 256.
-
-
-
+var mbOne = 1;
+var mbTwo = 2;
+(bitrate == 128)? alert("Song that have 128 may not have the best sound quality, but save a lot of space"): alert("Songs that have a bit rate higher than 128 will take up more space."); //ternary with ? question mark operator.
