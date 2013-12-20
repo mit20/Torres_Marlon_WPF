86aba7a36a871a503e65280ba8bf19bab7180bf3
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:46:49 -0500

    Added to comments on line 5.

--- Functions_Wacky/js/functions_wacky.js
+++ Functions_Wacky/js/functions_wacky.js
@@ -2,7 +2,7 @@
 //Problem: You're a waiter/waitress at a restaurant. Calculate what your average tip and if its a good or bad shift in terms of tips.
 
 var tipsGlobal = prompt("What is the number of tips you received today?", " ");//declares variable and asks user to enter amount of tips
-(tipsGlobal <= 5)? alert("Wow that's pretty bad!") : alert("That's not too bad!");//conditional statement - ternary.
+(tipsGlobal <= 5)? alert("Wow that's pretty bad!") : alert("That's not too bad!");//conditional statement - ternary. ? uses question mark operator.
 
 var moneyGlobal = prompt("What is the dollar amount you made in tips today?", " ");//declared and defined variable.  prompt asks user to enter the dollar amount they earned.
 var calcTips = function (tips,money){ //function with its two parameters.
