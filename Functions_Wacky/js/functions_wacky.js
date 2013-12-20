6d0a711f896613ec0c95ce29d6075de7c094c2a3
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:07:46 -0500

    Intial setup of index and javascript file for the Functions Wacky problem.

---
+++ Functions_Wacky/js/functions_wacky.js
@@ -0,0 +1,3 @@
+//Marlon Torres - December 19, 2013 - Web Programming Fundamentals - Functions Wacky
+//Problem: You're a waiter/waitress at a restaurant. Calculate what your average tip and if its a good or bad shift in terms of tips.
+
