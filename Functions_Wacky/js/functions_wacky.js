5a47c1d6e4ce2b0dd73065b2086bf863a5f51256
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:11:16 -0500

    Added ternary condition.

--- Functions_Wacky/js/functions_wacky.js
+++ Functions_Wacky/js/functions_wacky.js
@@ -2,3 +2,5 @@
 //Problem: You're a waiter/waitress at a restaurant. Calculate what your average tip and if its a good or bad shift in terms of tips.
 
 var tipsGlobal = prompt("What is the number of tips you received today?", " ");//declares variable and asks user to enter amount of tips
+(tipsGlobal <= 5)? alert("Wow that's pretty bad!") : alert("That's not too bad!");//conditional statement - ternary.
+
