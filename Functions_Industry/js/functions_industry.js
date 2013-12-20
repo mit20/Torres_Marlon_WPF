e1036cb89f7116c0777f21810d7d7dc68917299c
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 23:11:16 -0500

    Corrected line 8 and added another variable.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -5,5 +5,6 @@ alert("This calculator will calculate how many songs your iPod can hold based on
 alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute.");//Alerts user of bit rate information.
 var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? Please enter a number only.", " ");//declared and defined variable. Prompt asks user question for he/she to input.
 var bitrates = prompt("What bit rates are your songs?");//declared and defined variable bitrates. Prompt asks user question.
-var averageSongLength =("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
+var averageSongLength =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
+var songsPerGigabyte = 256;
 
