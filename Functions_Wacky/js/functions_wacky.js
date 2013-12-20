7edbce5f42530f98c30e2e3ac7207560134774f5
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 22:14:30 -0500

    Changed the question in the prompt.

--- Functions_Wacky/js/functions_wacky.js
+++ Functions_Wacky/js/functions_wacky.js
@@ -4,6 +4,6 @@
 var tipsGlobal = prompt("What is the number of tips you received today?", " ");//declares variable and asks user to enter amount of tips
 (tipsGlobal <= 5)? alert("Wow that's pretty bad!") : alert("That's not too bad!");//conditional statement - ternary.
 
-var moneyGlobal = prompt("Enter how much in tips you made?", " ");//declared and defined variable.  prompt asks user to enter the dollar amount they earned.
+var moneyGlobal = prompt("What is the dollar amount you made in tips today?", " ");//declared and defined variable.  prompt asks user to enter the dollar amount they earned.
 
 
