dc0e35cfb55685d6bdc2484326c51cde6396fa2d
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-20 01:04:33 -0500

    Changed variable names, added else line, console.log and moved variables from line 11 and 12 to 15 and 16 respectively.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -3,22 +3,25 @@
 
 alert("This calculator will calculate how many songs your iPod can hold based on the capacity of your iPod and bit rate of your songs.");//Shows this message to user.
 alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute).");//Alerts user of bit rate information.
-var songs = prompt("How many songs would you like to load on to your iPod?", " ");
+var songsGlobal = prompt("How many songs would you like to load on to your iPod?", " ");
 var gigabytes = prompt("How many gigabytes is your iPod? 8 gb, 16 gb or 32 gb? Please enter a number only.", " ");//declared and defined variable. Prompt asks user question for he/she to input.
-var bitrate = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bitrate. Prompt asks user question.
-var averageSongLength =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
+var bitrateGlobal = prompt("What bit rates are your songs? (Enter 128 or 256)");//declared and defined variable bitrate. Prompt asks user question.
+var averageSongLengthGlobal =prompt("What is the average length of each of your songs (minutes)? Please enter a whole number i.e. 3, 4 or 5.", " ");
 var songsPerGigabyte = 256;//variable is for songs per Gigabyte.  1024kb = 1 mb Avg. song is 4 min. so 1024/4 = 256.
-var mbOne = 1;
-var mbTwo = 2;
-(bitrate == 128)? alert("Song that have 128 may not have the best sound quality, but save a lot of space"): alert("Songs that have a bit rate higher than 128 will take up more space."); //ternary with ? question mark operator.
 
-function calcSongs(size, duration){
-    if(bitrate == "128"){
-        return (songs * averageSongLength) * mbOne;
+(bitrateGlobal == 128)? alert("Song that have 128 may not have the best sound quality, but save a lot of space"): alert("Songs that have a bit rate higher than 128 will take up more space."); //ternary with ? question mark operator.
+
+function calcSongs(songs, averageSongLength, bitrate){
+    var mbOne = 1;
+    var mbTwo = 2;
+
+    if(bitrate == 128){
+        return "If you upload " + songs + " it will take up " + (songs * averageSongLength) * mbOne + " megabytes.";
     }else if(bitrate == 256){
-        return (songs * averageSongLength) * mbTwo;
+        return "If you upload " + songs + " it will take up " + (songs * averageSongLength) * mbTwo + " megabytes.";
+    }else{
 
     }
 }
 
-
+console.log(calcSongs(songsGlobal, averageSongLengthGlobal, bitrateGlobal));//Prints to console.
