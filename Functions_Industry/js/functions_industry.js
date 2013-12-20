0016c94c5f2c136098c4516352f22566f53c2d1b
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 23:03:54 -0500

    Added alert and change comments.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -1,1 +1,4 @@
 //Marlon Torres - December 18, 2013 - Web Programming Fundamentals - Functions Industry
+//Problem: Calculate how many songs will fit on a 16 gigabyte iPod.
+
+alert("This calculator will calculate how many songs your iPod can hold based on the average length of your songs and bit rate of your songs.");//Shows this message to user.
