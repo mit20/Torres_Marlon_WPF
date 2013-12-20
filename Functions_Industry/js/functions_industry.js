9916442e7bb5b6f547a9b7d3bfbf51ee02be99c4
Marlon I. Torres <mitorres@fullsail.edu>
2013-12-19 23:05:44 -0500

    Added second alert.

--- Functions_Industry/js/functions_industry.js
+++ Functions_Industry/js/functions_industry.js
@@ -2,3 +2,4 @@
 //Problem: Calculate how many songs will fit on a 16 gigabyte iPod.
 
 alert("This calculator will calculate how many songs your iPod can hold based on the average length of your songs and bit rate of your songs.");//Shows this message to user.
+alert("Based on a 128 bit rate, the average song is about 4 megabytes (1 megabyte per minute of a song, for 256 bit rate its 2 megabytes per minute.");//Alerts user of bit rate information.
